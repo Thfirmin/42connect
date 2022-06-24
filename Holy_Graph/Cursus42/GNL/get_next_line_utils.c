54d3110f157c4a04b1ccaf901baa70cc5ab45af7
thfirmin <thiagofirmino2001@gmail.com>
2022-06-24 17:28:08 -0300

    get_next_line in progress

--- Holy_Graph/Cursus42/GNL/get_next_line_utils.c
+++ Holy_Graph/Cursus42/GNL/get_next_line_utils.c
@@ -1,3 +1,5 @@
+#include "get_next_line.h"
+
 int	ft_strlen(char *str)
 {
 	int	count;
@@ -8,4 +10,40 @@ int	ft_strlen(char *str)
 		count++;
 	}
 	return (count);
+}
+
+char	*ft_srtconcate(char *buffer, char *str)
+{
+	char	*newstr;
+	size_t	len;
+	int		index;
+
+	index = 0;
+	len = (ft_strlen(buffer) + ft_strlen(str));
+	newstr = malloc (len + 1);
+	if (!newstr)
+		return (0);
+	while (*(str + index))
+		*newstr++ = *(str + index++);
+	while (*buffer)
+		*newstr++ = *buffer++;
+	*newstr = '\0';
+	if	(str)
+		free(str);
+	return (newstr - len);
+}
+
+int	ft_detectline(char *str)
+{
+	s
+}
+
+char	*ft_cleanbuffer(char *str)
+{
+	s
+}
+
+char	*ft_getline(char *str)
+{
+	s
 }

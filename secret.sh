bf3e48c57b6cee7df54c6ec2c0cac614f87e7768
ThiagoFirmino2001 <thiagofirmino2001@gmal.com>
2022-06-26 03:32:53 +0100

    temp

---
+++ secret.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+sleep 3
+echo -e "\e[8mXXXXX\e[41mOOO\e[49mXXXXXX\e[41mOOO\e[49mXXXXX\e[m"
+sleep 1
+echo -e "\e[8mXXX\e[41mOOOOOOO\e[49mXX\e[41mOOOOOOO\e[49mXXX\e[m"
+sleep 1
+echo -e "\e[8mXXX\e[41mOOOOOOOOOOOOOOOO\e[49mXXX\e[m"
+sleep 1
+echo -e "\e[8mXXXXX\e[41mOOOOOOOOOOOO\e[49mXXXXX\e[m"
+sleep 1
+echo -e "\e[8mXXXXXXX\e[41mOOOOOOOO\e[49mXXXXXXX\e[m"
+sleep 1
+echo -e "\e[8mXXXXXXXXX\e[41mOOOO\e[49mXXXXXXXXX\e[m"
+sleep 1
+echo -e "\e[8mXXXXXXXXXX\e[41mOO\e[49mXXXXXXXXXX\e[m"

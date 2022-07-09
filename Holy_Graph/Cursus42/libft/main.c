15ab33f339c1c6c419cd0c28e88679b2b7de073d
ThiagoFirmino2001 <thiagofirmino2001@gmail.com>
2022-07-09 15:58:55 -0300

    update

--- Holy_Graph/Cursus42/libft/main.c
+++ Holy_Graph/Cursus42/libft/main.c
@@ -9,9 +9,10 @@ typedef struct s_list
 
 int	main(void)
 {
-7	t_list	**node;
+	t_list	**node;
 	node = (t_list *)malloc (sizeof(t_list) * 3);
-	*node -> fd = 42;
-	*node
+	**node -> fd = 42;
+	*(*node + 1) -> fd = 24;
+	*(*node + 2) -> fd = 54;
 	printf ("%d\n", (*node).fd);
 }
